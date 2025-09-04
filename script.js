0e2d3a199ee1a1019c408b09c669c565b7d282be
juliaknupp <165339819+juliaknupp@users.noreply.github.com>
2025-09-04 17:31:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,55 @@
-function listarTodosArguumentos() {
-  for (let i = 0; i < arguments.length; i++) {
-    console.log(arguments[i]);
-  }
-}
+const itens = [
+  {
+    nome: "Café Espresso",
+    preco: 8.0,
+    img: "https://uploads.metroimg.com/wp-content/uploads/2019/10/17184313/190115JB-Koppe-cafes-especiais-0121.jpg",
+  },
+  {
+    nome: "Capuccino",
+    preco: 12.0,
+    img: "https://www.minhareceita.com.br/app/uploads/2024/07/mobile-capuccino.jpg",
+  },
+  {
+    nome: "Suco de Laranja",
+    preco: 10.0,
+    img: "https://cardapio.primeirobar.com.br/wp-content/uploads/2022/04/laranja.jpg",
+  },
+  {
+    nome: "Água mineral",
+    preco: 6.0,
+    img: "https://fontagua.com.br/wp-content/uploads/2019/02/splash_350ml.jpg",
+  },
+  {
+    nome: "Fatia de bolo",
+    preco: 12.0,
+    img: "https://cdn.awsli.com.br/2500x2500/2658/2658919/produto/257217444/fatia-de-bolo-vegano-de-brigadeiro-com-morangos-sem-gl-ten-fnv2j0u5hp.png",
+  },
+  {
+    nome: "Sanduíche natural",
+    preco: 15.0,
+    img: "https://guiadacozinha.com.br/wp-content/uploads/2014/01/Sanduiche-natural-768x619.jpg",
+  },
+  {
+    nome: "Croissant",
+    preco: 17.0,
+    img: "https://www.fornodeminas.com.br/wp-content/uploads/2018/06/croissant.jpg",
+  },
+];
 
-listarTodosArguumentos(1, 2, 3);
+const cafet = document.getElementById("cardapio");
+let i =0;
+itens forEach item =>
+    let li = document.createElement('li');
+    li textContent = item nome;
+    cardapio.appendChild(li);
 
-function somar(...valores) {
-  let soma = 0;
-  const qtd = valores.length;
-  for (let i = 0; i < qtd; i++) {
-    soma += valores[i];
-  }
-  return soma;
-}
-console.log(somar(1, 2));
-console.log(somar(1, 2, 3));
-console.log(somar(1, 2, 3, 4));
+    let preco = document.createElement('p')
+    preco textContent ='R$'+ item preco;
+    li.appendChild(preco);
 
-function soma(a, b){
-    console.log(a +b);
-}
+    let img = document.createElement('img')
+    img scr =item img;
+    li.appendChild(img);
 
-soma(1, 2);
-
-var numeros = [1, 2];
-soma(...numeros);
+    let botao = document.createElement('button');
+    
